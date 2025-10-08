dc719e53de27e7e4506293daadf20a267ba7dd94
vitorotto <vitorotto05@gmail.com>
2025-10-07 23:29:09 -0300

    add: Inicio da estilização dá página de login do aplicativo

--- lib/pages/login.pages.dart
+++ lib/pages/login.pages.dart
@@ -6,61 +6,27 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text("Login")),
+      backgroundColor: Color(0xffF0FDF4),
       body: Center(
         child: SizedBox(
-          width: 250,
-          child: Column(
-            spacing: 30,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Entre no app",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Email",
-                ),
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Senha",
-                ),
-              ),
-              SizedBox(
-                width: 225,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 76, 175, 79),
-                        padding: EdgeInsets.all(10),
-                        maximumSize: Size(300.0, 50.0),
-                        minimumSize: Size(200.0, 50.0),
-                        shadowColor: Color.fromARGB(255, 255, 255, 255),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: const Text("Login com Google"),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          width: 350,
+          height: 600,
+          child: Container(
+            constraints: BoxConstraints.expand(
+              height:
+                  Theme.of(context).textTheme.headlineMedium!.fontSize! * 1.1 +
+                  200.0,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              border: Border.all(color: Colors.blueAccent),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Column(
+                
+            ),
           ),
         ),
       ),
