5fbecf41f5cf0dd038902dce99255d588a4d6736
vitorotto <vitorotto05@gmail.com>
2025-10-29 23:15:47 -0300

    add: Páginas de bem vindo para cada tipo de usuário

---
+++ lib/src/views/user/welcome_user_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class WelcomeUserPage extends StatelessWidget {
+  const WelcomeUserPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[Color(0xffF0FDF4), Color(0xffEFF6FF)],
+            ),
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Center(child: Text('Bem-vindo(a), Usuário(a)!')),
+        ),
+      ),
+    );
+  }
+}
