255c9edb7120664c0c9f044bfb64e8615150aaf2
vitorotto <vitorotto05@gmail.com>
2025-10-29 23:15:05 -0300

    feat: telas de login e cadastro funcionais

---
+++ lib/src/views/auth/cadastro_page.dart
@@ -0,0 +1,307 @@
+// lib/views/cadastro_page.dart (NOVO ARQUIVO)
+import 'package:flutter/material.dart';
+import 'package:g1_g2/src/views/auth/login_page.dart';
+import 'package:provider/provider.dart';
+import 'package:g1_g2/src/viewmodels/auth/cadastro_viewmodel.dart'; // Importe seu VM
+
+class CadastroPage extends StatelessWidget {
+  const CadastroPage({super.key});
+
+  // Widget de helper para um campo de texto padrão
+  Widget _buildTextField(
+    TextEditingController controller,
+    String label,
+    TextInputType type,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextField(
+        controller: controller,
+        keyboardType: type,
+        decoration: InputDecoration(
+          hintText: label,
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(width: 2, color: Color(0xff00A63E)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(style: BorderStyle.none),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Widget de helper para os campos condicionais
+  Widget _buildConditionalFields(CadastroViewModel vm) {
+    switch (vm.tipoSelecionado) {
+      case TipoUsuario.cliente:
+        return _buildTextField(vm.cpfController, "CPF", TextInputType.number);
+      case TipoUsuario.loja:
+        return Column(
+          children: [
+            _buildTextField(vm.cnpjController, "CNPJ", TextInputType.number),
+            _buildTextField(
+              vm.enderecoLojaController,
+              "Endereço da Loja",
+              TextInputType.text,
+            ),
+          ],
+        );
+      default:
+        return Container();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.watch<CadastroViewModel>();
+    final vmRead = context.read<CadastroViewModel>();
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[Color(0xffF0FDF4), Color(0xffEFF6FF)],
+            ),
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: BorderSide(color: Color(0x20000000)),
+                  ),
+                  shadowColor: Colors.transparent,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 30,
+                      bottom: 30,
+                      right: 20,
+                      left: 20,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          children: [
+                            TextButton.icon(
+                              style: TextButton.styleFrom(
+                                padding: EdgeInsets.only(left: 0, right: 0),
+                                iconColor: Colors.black,
+                              ),
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LoginPage(),
+                                  ),
+                                );
+                              },
+                              icon: Icon(Icons.arrow_back),
+                              label: Text(
+                                'Voltar',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Image.asset(
+                                'assets/images/logo2.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Criar conta', style: TextStyle(fontSize: 20)),
+                            Text(
+                              'Comece a contribuir com o meio ambiente',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xFF717182),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        SizedBox(height: 15),
+
+                        // Seletor de Tipo de Usuário
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 240, 237, 237),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              left: 4,
+                              right: 4,
+                              top: 1,
+                              bottom: 1,
+                            ),
+                            child: SegmentedButton<TipoUsuario>(
+                              segments: const [
+                                ButtonSegment(
+                                  value: TipoUsuario.cliente,
+                                  label: Text('Cliente'),
+                                ),
+                                ButtonSegment(
+                                  value: TipoUsuario.loja,
+                                  label: Text('Loja'),
+                                ),
+                                // Você pode remover o 'Admin' se o admin
+                                // não puder se cadastrar publicamente
+                                ButtonSegment(
+                                  value: TipoUsuario.admin,
+                                  label: Text('Admin'),
+                                ),
+                              ],
+                              selected: {vm.tipoSelecionado},
+                              onSelectionChanged:
+                                  (Set<TipoUsuario> newSelection) {
+                                    vmRead.setTipoUsuario(newSelection.first);
+                                  },
+                              showSelectedIcon: false,
+                              style:
+                                  SegmentedButton.styleFrom(
+                                    // Cores base
+                                    backgroundColor: Colors
+                                        .grey[200], // Fundo cinza (não selecionado)
+                                    selectedBackgroundColor: Colors
+                                        .white, // Fundo branco (selecionado)
+                                    selectedForegroundColor: Color(
+                                      0xFF00A63E,
+                                    ), // Texto verde (selecionado)
+                                  ).copyWith(
+                                    // Customizações adicionais
+
+                                    // 1. Remove a borda preta
+                                    side: WidgetStateProperty.all(
+                                      BorderSide.none,
+                                    ),
+
+                                    // 2. ARREDONDA TUDO (o contêiner e o botão selecionado)
+                                    shape: WidgetStateProperty.all(
+                                      RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                    ),
+                                  ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(height: 15),
+
+                        // --- Campos Comuns ---
+                        _buildTextField(
+                          vm.nomeController,
+                          "Nome Completo",
+                          TextInputType.name,
+                        ),
+                        _buildTextField(
+                          vm.emailController,
+                          "E-mail",
+                          TextInputType.emailAddress,
+                        ),
+                        _buildTextField(
+                          vm.passwordController,
+                          "Senha",
+                          TextInputType.visiblePassword,
+                        ),
+                        _buildTextField(
+                          vm.cityController,
+                          "Cidade",
+                          TextInputType.text,
+                        ),
+
+                        // --- Campos Condicionais ---
+                        _buildConditionalFields(vm),
+
+                        SizedBox(height: 15),
+                        // --- Botão de Cadastrar ---
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(vertical: 16),
+                            backgroundColor: Color(0xff00A63E),
+                          ),
+                          onPressed: vm.isLoading
+                              ? null
+                              : () async {
+                                  FocusScope.of(context).unfocus();
+                                  bool sucesso = await vmRead.cadastrar();
+
+                                  if (!context.mounted) return;
+
+                                  if (sucesso) {
+                                    // Sucesso! Mostra msg e volta p/ login
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                          "Cadastro realizado com sucesso!",
+                                        ),
+                                        backgroundColor: Colors.green,
+                                      ),
+                                    );
+                                    Navigator.pop(
+                                      context,
+                                    ); // Volta p/ tela de login
+                                  } else {
+                                    // Erro! Mostra msg
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                          vm.errorMessage ?? "Erro no cadastro",
+                                        ),
+                                        backgroundColor: Colors.red,
+                                      ),
+                                    );
+                                  }
+                                },
+                          child: vm.isLoading
+                              ? CircularProgressIndicator(color: Colors.white)
+                              : Text(
+                                  "Cadastrar",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/views/auth/login_page.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:g1_g2/src/views/admin/welcome_admin_page.dart';
+import 'package:g1_g2/src/views/store/welcome_store_page.dart';
+import 'package:g1_g2/src/views/user/welcome_user_page.dart';
+import 'package:provider/provider.dart';
+import 'package:g1_g2/src/viewmodels/auth/login_viewmodel.dart';
+import 'package:g1_g2/src/views/auth/cadastro_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    // 'watch' é ótimo para o 'build', pois reconstrói com 'isLoading'
+    final loginVM = context.watch<LoginViewModel>();
+    // 'read' é melhor para funções como 'onPressed'
+    final loginVMRead = context.read<LoginViewModel>();
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[Color(0xffF0FDF4), Color(0xffEFF6FF)],
+            ),
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: BorderSide(color: Color(0x20000000)),
+                  ),
+                  shadowColor: Colors.transparent,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 30,
+                      bottom: 30,
+                      right: 20,
+                      left: 20,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Image.asset(
+                            'assets/images/logo2.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text('Right EcoPoints', style: TextStyle(fontSize: 24)),
+                        Text(
+                          'Facilite a coleta de lixo sustentável',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Color(0xff717182),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        SizedBox(
+                          height: 30,
+                          width: double.infinity,
+                          child: Text('E-mail', style: TextStyle(fontSize: 17)),
+                        ),
+                        TextField(
+                          controller: loginVM.emailController,
+                          cursorColor: Color(0xff00A63E),
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: 'seu@email.com',
+                            filled: true,
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: Color(0xff00A63E),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(style: BorderStyle.none),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        SizedBox(
+                          height: 30,
+                          width: double.infinity,
+                          child: Text('Senha', style: TextStyle(fontSize: 17)),
+                        ),
+                        TextField(
+                          controller: loginVM.passwordController,
+                          obscureText: true,
+                          cursorColor: Color(0xff00A63E),
+                          decoration: InputDecoration(
+                            hintText: 'Sua senha',
+                            filled: true,
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: Color(0xff00A63E),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide(style: BorderStyle.none),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        ElevatedButton(
+                          onPressed: loginVM.isLoading
+                              ? null
+                              : () async {
+                                  // Esconde o teclado
+                                  FocusScope.of(context).unfocus();
+
+                                  // Pega o ViewModel para chamar a função (context.read)
+                                  final vm = context.read<LoginViewModel>();
+                                  bool sucesso = await loginVMRead.login();
+
+                                  if (!context.mounted) return;
+
+                                  if (sucesso) {
+                                    // 1. Pegamos o usuário que o ViewModel armazenou
+                                    final usuario = vm.usuarioLogado;
+
+                                    // 2. Criamos a rota (página) de destino
+                                    Widget paginaDestino;
+
+                                    if (usuario?.role == 'admin') {
+                                      // Se o usuário for Admin...
+                                      paginaDestino = WelcomeAdminPage();
+                                    } else if (usuario?.role == 'store') {
+                                      // Se for Loja...
+                                      paginaDestino = WelcomeStorePage();
+                                    } else if (usuario?.role == 'user') {
+                                      // Se for Cliente...
+                                      paginaDestino = WelcomeUserPage();
+                                    } else {
+                                      // Se for nulo ou desconhecido (não deve acontecer)
+                                      ScaffoldMessenger.of(
+                                        context,
+                                      ).showSnackBar(
+                                        SnackBar(
+                                          content: Text(
+                                            "Erro: Tipo de usuário desconhecido.",
+                                          ),
+                                          backgroundColor: Colors.red,
+                                        ),
+                                      );
+                                      return; // Para a execução
+                                    }
+
+                                    // 3. Navegamos para a página correta
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => paginaDestino,
+                                      ),
+                                    );
+                                  } else {
+                                    // Mostra o erro
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content: Text(
+                                          loginVM.errorMessage ??
+                                              "Erro desconhecido",
+                                        ),
+                                        backgroundColor: Colors.red,
+                                      ),
+                                    );
+                                  }
+                                },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            backgroundColor: Color(0xff00A63E),
+                            iconColor: Colors.white,
+                            shadowColor: Colors.transparent,
+                          ),
+
+                          child: loginVM.isLoading
+                              ? CircularProgressIndicator(color: Colors.white)
+                              : Text("Entrar"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Não possui uma conta?"),
+                    TextButton(
+                      child: Text(
+                        "Cadastre-se",
+                        style: TextStyle(
+                          color: Color(0xff00A63E),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Limpa qualquer erro de login antes de ir
+                        loginVMRead.setError(null);
+
+                        // Navega para a tela de Cadastro
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CadastroPage(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
