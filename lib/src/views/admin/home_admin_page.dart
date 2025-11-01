9362eeee908b71ec7af65d8914c0a0d86110c6a2
vitorotto <vitorotto05@gmail.com>
2025-11-01 00:48:00 -0300

    style: Ajustando layout das telas usando componentes de estilização inicial das telas

--- lib/src/views/admin/home_admin_page.dart
+++ lib/src/views/admin/home_admin_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:g1_g2/components/custom_green_button.dart';
+import 'package:g1_g2/components/custom_initial_layout.dart';
 import 'package:g1_g2/components/custom_voltar_text_buttom.dart';
 import 'package:g1_g2/src/repositories/auth_repository.dart';
+import 'package:g1_g2/src/views/admin/add_collect_point_form_page.dart';
 import 'package:g1_g2/src/views/admin/welcome_admin_page.dart';
 import 'package:g1_g2/src/views/auth/login_page.dart';
 import 'package:provider/provider.dart';
@@ -10,39 +13,53 @@ class HomeAdminPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: <Color>[Color(0xffF0FDF4), Color(0xffEFF6FF)],
-            ),
-          ),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: SafeArea(
-            child: Column(
+    return CustomInitialLayout(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0),
+            child: Row(
               children: [
                 CustomVoltarTextButtom(pageToBack: WelcomeAdminPage()),
-                Text('Menu do Administrador!'),
-                ElevatedButton(
-                  onPressed: () async {
-                    await context.read<AuthRepository>().signOut();
-                    if (context.mounted) {
-                      Navigator.of(context).pushAndRemoveUntil(
-                        MaterialPageRoute(builder: (_) => const LoginPage()),
-                        (route) => false,
-                      );
-                    }
-                  },
-                  child: const Text('Logout'),
-                ),
               ],
             ),
           ),
-        ),
+          Text(
+            'Pontos de coleta do município',
+            style: TextStyle(fontSize: 18, color: Color(0xFF0A0A0A)),
+          ),
+          SizedBox(height: 25),
+          Text(
+            'Todos os pontos de coleta existentes no município',
+            style: TextStyle(fontSize: 18, color: Color(0xFF717182)),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 25),
+          ElevatedButton(
+            onPressed: () async {
+              await context.read<AuthRepository>().signOut();
+              if (context.mounted) {
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (_) => const LoginPage()),
+                  (route) => false,
+                );
+              }
+            },
+            child: const Text('Logout'),
+          ),
+          CustomGreenButton(
+            handleClick: () {
+              // Use push so that the form can pop back to this page after saving
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AddCollectPointFormPage(),
+                ),
+              );
+            },
+            label: 'Cadastrar novo ponto',
+          ),
+        ],
       ),
     );
   }
--- lib/src/views/admin/welcome_admin_page.dart
+++ lib/src/views/admin/welcome_admin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:g1_g2/components/custom_green_button.dart';
+import 'package:g1_g2/components/custom_initial_layout.dart';
 import 'package:g1_g2/src/views/admin/home_admin_page.dart';
 
 class WelcomeAdminPage extends StatelessWidget {
@@ -7,105 +8,80 @@ class WelcomeAdminPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: <Color>[Color(0xffF0FDF4), Color(0xffEFF6FF)],
+    return CustomInitialLayout(
+      child: Column(
+        children: [
+          SizedBox(height: 60),
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[Color(0xFF00C950), Color(0xff2B7FFF)],
+              ),
             ),
+            child: Icon(Icons.shield_outlined, size: 60, color: Colors.white),
           ),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: SafeArea(
+          SizedBox(height: 20),
+          Text(
+            'Bem-vindo, \nAdministrador!',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Color(0xFF008236), fontSize: 24),
+          ),
+          SizedBox(height: 20),
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+              side: BorderSide(color: Color(0xFFB9F8CF), width: 2),
+            ),
+            shadowColor: Colors.transparent,
+            color: Colors.transparent,
             child: Padding(
-              padding: const EdgeInsets.all(12.0),
+              padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: [
-                  SizedBox(height: 60),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: <Color>[Color(0xFF00C950), Color(0xff2B7FFF)],
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.store_rounded,
+                        size: 30,
+                        color: Color(0xFF00A63E),
                       ),
-                    ),
-                    child: Icon(
-                      Icons.shield_outlined,
-                      size: 60,
-                      color: Colors.white,
-                    ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Administre os pontos de coleta',
+                        style: TextStyle(
+                          fontSize: 19,
+                          color: Color(0xFF0A0A0A),
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 10),
                   Text(
-                    'Bem-vindo, \nAdministrador!',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Color(0xFF008236), fontSize: 24),
+                    'Veja os pontos de coleta e ajuste eles conforme a necessidade do município',
+                    style: TextStyle(fontSize: 17, color: Color(0xFF717182)),
                   ),
-                  SizedBox(height: 20),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      side: BorderSide(color: Color(0xFFB9F8CF), width: 2),
-                    ),
-                    shadowColor: Colors.transparent,
-                    color: Colors.transparent,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.store_rounded,
-                                size: 30,
-                                color: Color(0xFF00A63E),
-                              ),
-                              SizedBox(width: 10),
-                              Text(
-                                'Administre os pontos de coleta',
-                                style: TextStyle(
-                                  fontSize: 19,
-                                  color: Color(0xFF0A0A0A),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            'Veja os pontos de coleta e ajuste eles conforme a necessidade do município',
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: Color(0xFF717182),
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                          CustomGreenButton(
-                            label: 'Visualizar pontos',
-                            handleClick: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => HomeAdminPage(),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
+                  SizedBox(height: 30),
+                  CustomGreenButton(
+                    label: 'Visualizar pontos',
+                    handleClick: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeAdminPage(),
+                        ),
+                      );
+                    },
                   ),
                 ],
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
