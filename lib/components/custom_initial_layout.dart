c56cc967ef6f2f61639c04bc31bd206cbbe6fe6e
vitorotto <vitorotto05@gmail.com>
2025-11-01 16:18:45 -0300

    fix: initial layout não aplica mais tamanho infinito

--- lib/components/custom_initial_layout.dart
+++ lib/components/custom_initial_layout.dart
@@ -8,20 +8,18 @@ class CustomInitialLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: <Color>[Color(0xffF0FDF4), Color(0xffEFF6FF)],
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[Color(0xffF0FDF4), Color(0xffEFF6FF)],
           ),
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: SafeArea(
-            child: Padding(padding: const EdgeInsets.all(12.0), child: child),
-          ),
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SafeArea(
+          child: Padding(padding: const EdgeInsets.all(12.0), child: child),
         ),
       ),
     );
